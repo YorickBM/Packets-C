13e56720bafe03d61aea125532517486b930b641
YorickBM <yorickblom@hotmail.com>
2022-09-05 19:08:06 +0200

    Update socket_client.h
    Allow few client & server settings in this file.
    IMPORTANT! Always give every client a unique id!!!

--- lib/socket_client.h
+++ lib/socket_client.h
@@ -1,30 +1,22 @@
-﻿#ifndef SOCKET_CLIENT_H
+#ifndef SOCKET_CLIENT_H
 #define SOCKET_CLIENT_H
 
 #include "src/packet_reader.h"
 #include "src/packet_definitions.h"
 
-int send_packet_floor(Packet*);
-int send_message_floor(char*);
-int send_packet_elevator(Packet*);
-int send_message_elevator(char*);
+int send_packet_all_client(Packet*);
+int send_packet_client(Packet* packet, char* id);
+int send_message_client(char* data, char* id);
 int send_packet_server(Packet*);
 int send_message_server(char*);
 unsigned __stdcall listener(void* arg);
 unsigned __stdcall message_handler(void* arg);
 int create_connection(char*, int);
+int delete_connection(char*);
 
 #define MAX_NUM_CLIENTS 80
 #define MAX_CLIENT_MSG_LEN 1000
-//#define SERVER
-#define ID ThisIsMyId
-
-int index = 0;
-int registery_index = 0;
-int server_running = 1;
-
-char clientMessage[MAX_CLIENT_MSG_LEN];
-int clientMessageLength;
-char* message;
+#define SERVER
+#define ID "PUT-YOUR-ID-HERE"
 
 #endif
